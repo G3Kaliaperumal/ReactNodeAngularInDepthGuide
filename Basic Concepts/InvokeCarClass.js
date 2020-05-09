0b6302d3cc85ef4b7a31e2a9268fab16d6225c5e
Kaliaperumal <Gayathri.Kaliaperumal@nielsen.com>
2020-05-10 01:45:33 +0530

    Lecture 23 - Custom Iterators

--- Basic Concepts/InvokeCarClass.js
+++ Basic Concepts/InvokeCarClass.js
@@ -1,5 +1,7 @@
-const Car = require('./Car');
-const RacingCar = require('./RacingCar');
+const Car = require('./src/Car/Car');
+const RacingCar = require('./src/Car/RacingCar');
+
+const iterator = require('./lib/CustomIterator');
 
 function runApp(){
     // Array
@@ -19,7 +21,7 @@ function runApp(){
     const car2 = new Car(carBrands[2], 'Lancer', 2010);
 
     // Prototypes
-    console.log('PROTOTYPE property:-');
+    console.log('PROTOTYPE PROPERTY:-');
     console.log(Car.prototype === car1.__proto__);
     console.log(Car.prototype === car2.__proto__);
 
@@ -27,24 +29,28 @@ function runApp(){
     const cars = [car1, car2, racingCar];
     
     // Usage of forEach
-    console.log('FOR EACH function:-');
+    console.log('FOR EACH FUNCTION:-');
     cars.forEach(function(car){
         car.displayCarInfo();
     });
 
     // Usage of map function
-    console.log('MAP function:-');
+    console.log('MAP FUNCTION:-');
     const carTypes = cars.map(function(car){
         return car.type;
     });
     console.log(carTypes);
 
     // Usage of filter function
-    console.log('FILTER function:-');
+    console.log('FILTER FUNCTION:-');
     const fordCars = cars.filter(function(car){
         return carBrands[1] == car.brand;
     });
     console.log(fordCars);
+
+    // Custom Iteration method
+    console.log('CUSTOM ITERATORS:-')
+    iterator.forEach(cars);
 }
 
 runApp();
---
+++ Basic Concepts/lib/CustomIterator.js
@@ -0,0 +1,11 @@
+const iterator = (function(){
+    return {
+        forEach: function(list) {
+            for(let i = 0; i < list.length; i++){
+                console.log(list[i]);
+            }
+        }
+    }
+})();
+
+module.exports = iterator;
