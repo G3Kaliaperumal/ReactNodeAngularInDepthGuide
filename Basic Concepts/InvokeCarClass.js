be472f6be0c17be790e9ffda583e935b7d41179a
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-09 19:48:14 +0530

    Lecture 22 - forEach, map and filter methods

--- Basic Concepts/InvokeCarClass.js
+++ Basic Concepts/InvokeCarClass.js
@@ -10,22 +10,41 @@ function runApp(){
     carBrands.push('Mitsubishi');
 
     // Non-Parameterized constructor
-    const car = new Car();
-    car.brand = carBrands[1];
-    car.type = 'Freestyle';
-    car.produceYear = 2019;
-    car.displayCarInfo();
+    const car1 = new Car();
+    car1.brand = carBrands[1];
+    car1.type = 'Freestyle';
+    car1.produceYear = 2019;
 
     // Parameterized constructor
     const car2 = new Car(carBrands[2], 'Lancer', 2010);
-    car2.displayCarInfo();
 
     // Prototypes
-    console.log(Car.prototype === car.__proto__);
+    console.log('PROTOTYPE property:-');
+    console.log(Car.prototype === car1.__proto__);
     console.log(Car.prototype === car2.__proto__);
 
     const racingCar = new RacingCar('k1', carBrands[0], 'Benz', 1990);
-    racingCar.displayCarInfo();
+    const cars = [car1, car2, racingCar];
+    
+    // Usage of forEach
+    console.log('FOR EACH function:-');
+    cars.forEach(function(car){
+        car.displayCarInfo();
+    });
+
+    // Usage of map function
+    console.log('MAP function:-');
+    const carTypes = cars.map(function(car){
+        return car.type;
+    });
+    console.log(carTypes);
+
+    // Usage of filter function
+    console.log('FILTER function:-');
+    const fordCars = cars.filter(function(car){
+        return carBrands[1] == car.brand;
+    });
+    console.log(fordCars);
 }
 
 runApp();
