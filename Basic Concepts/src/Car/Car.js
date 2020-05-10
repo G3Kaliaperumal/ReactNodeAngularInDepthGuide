4d42002c77398f8aee32a767adb472c101455c55
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-10 23:47:54 +0530

    Update Car.js

--- Basic Concepts/src/Car/Car.js
+++ Basic Concepts/src/Car/Car.js
@@ -2,15 +2,17 @@ class Car {
     brand = '';
     type = '';
     produceYear = null;
+    id = null;
 
     constructor(brand, type, produceYear) {
         this.brand = brand;
         this.type = type;
         this.produceYear = produceYear;
+        this.id = '_' + Math.random().toString(36).substr(2, 9); // Base 36
     }
 
     displayCarInfo(category) {
-        console.log(`Brand name: ${this.brand}, Type: ${this.type}, Year: ${this.produceYear}`
+        console.log(`${this.id}:- Brand name: ${this.brand}, Type: ${this.type}, Year: ${this.produceYear}`
         + (undefined !== category ? `, Category: ${category}` : ``));
     }
 }
