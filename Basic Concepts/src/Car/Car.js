3378fbb97daffd9369bb3895dc63ef73982dffb5
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-11 23:44:19 +0530

    Update Car.js
    Set current date for each car using CustomIterator function

--- Basic Concepts/src/Car/Car.js
+++ Basic Concepts/src/Car/Car.js
@@ -1,3 +1,5 @@
+const iterator = require('../../lib/CustomIterator');
+
 class Car {
     brand = '';
     type = '';
@@ -11,8 +13,14 @@ class Car {
         this.id = '_' + Math.random().toString(36).substr(2, 9); // Base 36
     }
 
+    setCarDate() {
+      // Using anonymous function like "function (date) { this.carDate = date; }" throws 'this' undefined error.
+      // Arrow function does not cause such error. You can also use bind() to avoid such error
+      iterator.getCurrentDate(date => this.carDate = date );
+    }
+
     displayCarInfo(category) {
-        console.log(`${this.id}:- Brand name: ${this.brand}, Type: ${this.type}, Year: ${this.produceYear}`
+        console.log(`${this.id}:- Brand name: ${this.brand}, Type: ${this.type}, Year: ${this.produceYear}, Date: ${this.carDate}`
         + (undefined !== category ? `, Category: ${category}` : ``));
     }
 }
