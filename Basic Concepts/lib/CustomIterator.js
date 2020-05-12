cc45700436bf064b0355a00203a79c2e83aa726b
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-12 22:45:48 +0530

    Update CustomIterator.js: Including promise object

--- Basic Concepts/lib/CustomIterator.js
+++ Basic Concepts/lib/CustomIterator.js
@@ -1,12 +1,23 @@
 const iterator = (function() {
     return {
         // To replicate back-end service calls
-        getDBdata: function(callback) {
+        getMethod1DBdata: function(callback) {
           // asynchronous function calls
           setTimeout(() => {
             const data = 'Data from DB!';
             callback(data);
           }, 3000);
+        },
+         getMethod2DBdata: function() {
+          // Promise is an object that returns value some time in the future
+          return new Promise((resolve, reject) => {
+            setTimeout(() => {
+              const data = 'Data from DB!';
+              resolve(data);
+              // const errorMessage = 'Cannot connect to DB!';
+              // reject(errorMessage);
+            }, 3000);
+          });
         },
         getCurrentDate: function(callback) {
           const date = new Date().toLocaleString();
