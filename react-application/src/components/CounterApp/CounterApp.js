54ad65945c69107a4171a51d1d8506c300a8b825
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-16 23:23:02 +0530

    Lecture 46 - useState

--- react-application/src/components/CounterApp/CounterApp.js
+++ react-application/src/components/CounterApp/CounterApp.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CounterApp.css';
 
 function CounterApp() {
+  /**
+   * useState(0) => return two values
+   * 1. Value given in the argument. In this case value '0'.
+   * 2. Function that helps mutate the value
+   */
+  const [count, setCount] = useState(0);
 
-  const increment = () => {
-    alert('Increment');
-  }
-
-  const decrement = () => {
-    alert('Decrement');
-  }
-
+  const modifyValue = (step) => () => setCount(count + step);
+  
   return (
     <div className='counter-app'>
-      <h1 className='value'>0</h1>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <h1 className='value'>{count}</h1>
+      <button onClick={modifyValue(1)}>Increment</button>
+      <button onClick={modifyValue(-1)}>Decrement</button>
     </div>
   );
 }
