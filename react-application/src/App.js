7a13fa752451791dc9d2c6d6128b97d98c4e8c70
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-16 18:35:37 +0530

    Update App.js

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './App.css';
 
 // Functional Component
 function App() {
   return (
-    <div className="App">
-      <h1> Hello World </h1>
+    <div>
+      <h1 className='title'> HAPPY.. </h1>
     </div>
   );
 }
