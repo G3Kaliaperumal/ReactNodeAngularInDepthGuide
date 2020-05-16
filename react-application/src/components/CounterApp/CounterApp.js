f2d50fad64dc04757789cf5177439a92b58e88ba
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-16 23:47:16 +0530

    Lecture 47 - Replace Fnctl cmpt with Class cmpt

--- react-application/src/components/CounterApp/CounterApp.js
+++ react-application/src/components/CounterApp/CounterApp.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './CounterApp.css';
 
-function CounterApp() {
-  /**
-   * useState(0) => return two values
-   * 1. Value given in the argument. In this case value '0'.
-   * 2. Function that helps mutate the value
-   */
-  const [count, setCount] = useState(0);
+// Class Component
+class CounterApp extends React.Component {
 
-  const modifyValue = (step) => () => setCount(count + step);
-  
-  return (
-    <div className='counter-app'>
-      <h1 className='value'>{count}</h1>
-      <button onClick={modifyValue(1)}>Increment</button>
-      <button onClick={modifyValue(-1)}>Decrement</button>
-    </div>
-  );
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    }
+  }
+
+  modifyValue = (step) => () => {
+    this.setState({
+      count: this.state.count + step
+    });
+  }
+
+  // Lifecycle function
+  render() {
+    const { count } = this.state;
+    return (
+      <div className='counter-app'>
+        <h1 className='value'>{count}</h1>
+        <button onClick={this.modifyValue(1)}>Increment</button>
+        <button onClick={this.modifyValue(-1)}>Decrement</button>
+      </div>
+    );
+  }
 }
 
 export default CounterApp;
