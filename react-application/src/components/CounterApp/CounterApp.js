d7760980ebed9d7edad4b9830b274f4a8bba46a0
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-17 15:48:08 +0530

    Update CounterApp.js - PropTypes

--- react-application/src/components/CounterApp/CounterApp.js
+++ react-application/src/components/CounterApp/CounterApp.js
@@ -1,5 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './CounterApp.css';
+
+/*
+import { useState } from 'react';
+
+// Functional Component
+const CounterApp = (props) => {
+  const [count, setCount] = useState(0);
+  const { title } = props;
+
+  const modifyValue = (step) => () => setCount(count + step);
+
+  return (
+    <div className='counter-app'>
+        <h2>{title}</h2>
+        <h1 className='value'>{count}</h1>
+        <button onClick={modifyValue(1)}>Increment</button>
+        <button onClick={modifyValue(-1)}>Decrement</button>
+      </div>
+  );
+}*/
 
 // Class Component
 class CounterApp extends React.Component {
@@ -20,14 +41,21 @@ class CounterApp extends React.Component {
   // Lifecycle function
   render() {
     const { count } = this.state;
+    const { title } = this.props;
     return (
       <div className='counter-app'>
+        <h2>{title}</h2>
         <h1 className='value'>{count}</h1>
         <button onClick={this.modifyValue(1)}>Increment</button>
         <button onClick={this.modifyValue(-1)}>Decrement</button>
       </div>
     );
   }
+}
+
+// Required for both Fnctl and class cmpt
+CounterApp.propTypes = {
+  title: PropTypes.string.isRequired
 }
 
 export default CounterApp;
