611ed82a0b1e3e15bb6a544b62f4d8847b570f16
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-05-28 20:36:58 +0530

    Update index.js

--- react-application/src/index.js
+++ react-application/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+import './index.css';
 
 const domElement = document.getElementById('root');
 // JSX - Javascript XML
