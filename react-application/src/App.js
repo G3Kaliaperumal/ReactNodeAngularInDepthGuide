a9f32f30fa1dfdadf07664e5239fcd860aa95515
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-07 18:02:19 +0530

    Update App.js

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import './App.css';
 import Header from './components/shared/Header';
+
 import RentalHome from './pages/RentalHome';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 // Functional Component
 function App() {
+
+  const renderPages= () => {
+    const { pathname } = window.location;
+
+    switch(pathname) {
+      case '/':
+        return <RentalHome />;
+      case '/login':
+        return <Login />;
+      case '/register':
+        return <Register />;
+      default:
+        return <RentalHome />;
+    }
+  }
+
   return (
     <div>
         <Header />
-        <RentalHome />
+        { renderPages() }
     </div>
   );
 }
