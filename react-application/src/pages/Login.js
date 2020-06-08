6f0d7cc5f051de820258288fad7b1f13533c28d5
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-08 23:49:46 +0530

    Update Login.js

--- react-application/src/pages/Login.js
+++ react-application/src/pages/Login.js
@@ -3,7 +3,13 @@ import React from 'react';
 import connect from '../store/connect';
 
 class Login extends React.Component {
-  render = () => <p>{JSON.stringify(this.props.rentals())}</p>
+  render = () => <p>{JSON.stringify(this.props.data1())}</p>
 }
 
-export default connect(Login);
+const mapStateToProps = (state) => {
+  return {
+    data1: state.data1
+  }
+}
+
+export default connect(mapStateToProps)(Login);
