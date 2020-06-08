44a43529bf459372f76cb5ff594f021e5c61e11a
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-08 21:14:22 +0530

    Update App.js

--- react-application/src/App.js
+++ react-application/src/App.js
@@ -3,15 +3,19 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import Header from './components/shared/Header';
 import Routes from './Routes';
+import { StateContext } from './state-context';
+import store from './store/rentals';
 
 
 // Functional Component
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes />
-    </Router>
+    <StateContext.Provider value={store}>
+      <Router>
+        <Header />
+        <Routes />
+      </Router>
+    </StateContext.Provider>
   );
 }
 
