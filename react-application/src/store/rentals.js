565d8954de1c1978996ff0d2db61ee5619266608
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-08 23:47:49 +0530

    Update rentals.js

--- react-application/src/store/rentals.js
+++ react-application/src/store/rentals.js
@@ -3,7 +3,9 @@ import { rentalData } from './data';
 const store = {
   rentals: () => {
     return rentalData;
-  }
+  },
+  data1: () => ['Happy', 'Coding', 'Babe!'],
+  data2: () => [1, 2, 3]
 }
 
 export default store;
