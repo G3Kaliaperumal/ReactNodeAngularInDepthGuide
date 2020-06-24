5a98697faaa01b47986b19a28c681442ee5b6bf3
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-06-24 20:00:36 +0530

    Lecture 358: DB setup

--- react-application/server/index.js
+++ react-application/server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./routes/rentals')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Replace <password> with the actual password
+mongoose.connect('mongodb+srv://GayathriK:<password>@cluster0-swbtm.mongodb.net/bookhotel?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, () => {
+  console.log('Success');
+});
 
 app.use(bodyParser.json())
 app.use('/api/v1/rentals', router);
