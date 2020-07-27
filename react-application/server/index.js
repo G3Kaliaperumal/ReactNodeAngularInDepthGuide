421fe2b195e7708ff03887e3c6993e4d14f8cd38
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-07-27 19:28:57 +0530

    Lecture 359 - Rental Model

--- react-application/server/index.js
+++ react-application/server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+
 const router = require('./routes/rentals')
+const config = require('./config/dev')
+const Rental = require('./models/rentals')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Replace <password> with the actual password
-mongoose.connect('mongodb+srv://GayathriK:<password>@cluster0-swbtm.mongodb.net/bookhotel?retryWrites=true&w=majority', {
+mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }, () => {
