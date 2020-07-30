6d3834fd75f139c5b61d9254c0bd6a17eb64c378
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-07-30 17:06:29 +0530

    Lecture 86 - Rental details refactor

--- react-application/src/pages/RentalDetail.js
+++ react-application/src/pages/RentalDetail.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { fetchRentalById } from 'actions/rentals';
-import { capitalize } from 'helpers/functions';
+import RentalInfo from 'components/rental/RentalInfo';
 
 class RentalDetail extends React.Component {
 
   componentDidMount() {
     const { id } = this.props.match.params;
-    this.props.dispatch(fetchRentalById(id));
+    this.props.dispatch(fetchRentalById(id))
   }
 
   render() {
     const { rental, isFetching } = this.props;
     if (isFetching) { return null; }
-
     return (
       <section id="rentalDetails">
         <div className="upper-section">
@@ -28,59 +26,10 @@ class RentalDetail extends React.Component {
             </div>
           </div>
         </div>
-
         <div className="details-section">
           <div className="row">
             <div className="col-md-8">
-              <div className="rental">
-                <h2 className={`rental-type type-${rental.category}`}>
-                  {rental.shared ? 'Shared' : 'Whole'} {rental.category}
-                </h2>
-                <h1 className="rental-title">{rental.title}</h1>
-                <h2 className="rental-city">{capitalize(rental.city)}</h2>
-                <div className="rental-room-info">
-                  <span>
-                    <FontAwesomeIcon icon="building" /> {rental.numOfRooms} bedrooms
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon="user" /> {rental.numOfRooms + 4} guests
-                  </span>
-                  <span>
-                    <FontAwesomeIcon icon="bed" /> {rental.numOfRooms + 2} beds
-                  </span>
-                </div>
-                <p className="rental-description">
-                  {rental.description}
-                </p>
-                <hr />
-                <div className="rental-assets">
-                  <h3 className="title">Assets</h3>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <span>
-                        <FontAwesomeIcon icon="asterisk" /> Cooling
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="thermometer" /> Heating
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="location-arrow" /> Iron
-                      </span>
-                    </div>
-                    <div className="col-md-6">
-                      <span>
-                        <FontAwesomeIcon icon="desktop" /> Working area
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="archive" /> Washing machine
-                      </span>
-                      <span>
-                        <FontAwesomeIcon icon="cube" /> Dishwasher
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <RentalInfo rental={rental} />
             </div>
             <div className="col-md-4"> BOOKING</div>
           </div>
