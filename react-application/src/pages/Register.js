af6620ff946db202f479f3771ef3c91feab026af
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-08-01 17:13:52 +0530

    Lecture 92 - Register form

--- react-application/src/pages/Register.js
+++ react-application/src/pages/Register.js
@@ -1,35 +1,16 @@
 import React from 'react';
+import RegisterForm from '../components/forms/RegisterForm';
 
 class Register extends React.Component {
+  registerUser = (registerData) => alert(JSON.stringify(registerData));
+
   render() {
     return (
       <div className="bwm-form">
         <div className="row">
           <div className="col-md-5">
             <h1 className="page-title">Register</h1>
-            <form>
-              <div className="form-group">
-                <label htmlFor="username">Username</label>
-                <input type="text" className="form-control" id="username" />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input type="email" className="form-control" id="email" />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input type="password" className="form-control" id="password" />
-              </div>
-
-              <div className="form-group">
-                <label htmlFor="passwordConfirmation">Confirm Password</label>
-                <input type="password" className="form-control" id="passwordConfirmation" />
-              </div>
-
-              <button type="submit" className="btn btn-outline-success my-2 my-sm-0">Submit</button>
-            </form>
+            <RegisterForm onSubmit={this.registerUser} />
           </div>
           <div className="col-md-6 ml-auto">
             <div className="image-container">
