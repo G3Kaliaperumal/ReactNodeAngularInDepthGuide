99a4e05237cbe9e51e696948e8673667ab557462
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-08-04 23:52:59 +0530

    Lecture 368 - Registering User

--- react-application/server/routes/users.js
+++ react-application/server/routes/users.js
@@ -4,7 +4,7 @@ const {
   login,
   register } = require('../controllers/users');
 
-router.get('/login', login);
-router.get('/register', register);
+router.post('/login', login);
+router.post('/register', register);
 
 module.exports = router;
