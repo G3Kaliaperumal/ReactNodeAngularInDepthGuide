8bef6a777115ec5c023802aa042bd7ebf6deae25
G3Kaliaperumal <kaliaperumalgayathri27@gmail.com>
2020-08-06 23:07:29 +0530

    Lecture 371 - JWT Intro

--- react-application/server/config/dev.js
+++ react-application/server/config/dev.js
@@ -1,3 +1,4 @@
 module.exports = {
-  DB_URI: 'mongodb+srv://GayathriK:password@cluster0-swbtm.mongodb.net/bookhotel?retryWrites=true&w=majority'
+  DB_URI: 'mongodb+srv://GayathriK:password@cluster0-swbtm.mongodb.net/bookhotel?retryWrites=true&w=majority',
+  JWT_SECRET: 'asda%33434'
 }
